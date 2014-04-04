370a20acde3de8161372a4fd1132edbd253d64cb
vincent4vx <quatrevieux.vincent@gmail.com>
2014-04-04 15:05:59 +0200

    ADD Packet annotation

---
+++ src/org/pearemu/network/Packet.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.pearemu.network;
+
+import java.lang.annotation.Documented;
+
+/**
+ *
+ * @author Vincent Quatrevieux <quatrevieux.vincent@gmail.com>
+ */
+@Documented
+public @interface Packet {
+    
+}
