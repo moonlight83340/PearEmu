f5e8df35e6510fa0a120fbaedc1e528a1fbf9d21
vincent4vx <quatrevieux.vincent@gmail.com>
2014-04-04 11:50:59 +0200

    ADD RealmPacketEnum

--- src/org/pearemu/commons/Constants.java
+++ src/org/pearemu/commons/Constants.java
@@ -2,6 +2,5 @@ package org.pearemu.commons;
 
 public class Constants {
 	final static public int DECO_TIME = 900;/*time in seconds(15 minutes)*/
-	
-
+	final static public String DOFUS_VER = "1.29.1";
 }
