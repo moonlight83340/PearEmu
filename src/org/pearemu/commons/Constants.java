891af4477b4fba7f4927e27c0b6e910fe8dcc781
moonlight83340 <naruto-hinatalove@hotmail.fr>
2014-04-07 22:13:06 +0200

    Update Constants.java

--- src/org/pearemu/commons/Constants.java
+++ src/org/pearemu/commons/Constants.java
@@ -1,5 +1,10 @@
 package org.pearemu.commons;
 
+/**
+ *
+ * @author moonlight83340,vincent4vx
+ */
+ 
 public class Constants {
 	final static public int DECO_TIME = 900;/*time in seconds(15 minutes)*/
 	final static public String DOFUS_VER = "1.29.1";
